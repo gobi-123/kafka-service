1775dfb4afdb809989a78a8bd79294dd3dd6fc0d
svudya <vudhyasaideepak@gmail.com>
2020-02-21 11:17:39 +0530

    added consumer config

--- src/main/java/com/p3solutions/kafka/configurations/MessageConsumerConfig.java
+++ src/main/java/com/p3solutions/kafka/configurations/MessageConsumerConfig.java
@@ -6,14 +6,17 @@ import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.DefaultJackson2JavaTypeMapper;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.kafka.support.converter.Jackson2JavaTypeMapper.TypePrecedence;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer2;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,19 +25,18 @@ import lombok.extern.slf4j.Slf4j;
  * MessageConsumerConfig
  */
 @Configuration
-@EnableKafka
+// @EnableKafka
 @Slf4j
 public class MessageConsumerConfig {
 
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapServer;
 
-    @Bean
+    // @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         log.info("Setting up consumer factory");
         Map<String, Object> props = new HashMap<String, Object>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "taskgroup");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer2.class);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer2.class);
@@ -44,17 +46,26 @@ public class MessageConsumerConfig {
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new StringDeserializer());
     }
 
-    @Bean
-    RecordMessageConverter messageConverter() {
-        return new StringJsonMessageConverter();
-    }
+    // @Bean
+    // RecordMessageConverter messageConverter() {
+    // log.info("Record Message Converter setup");
+    // StringJsonMessageConverter converter = new StringJsonMessageConverter();
+    // DefaultJackson2JavaTypeMapper typeMapper = new
+    // DefaultJackson2JavaTypeMapper();
+    // typeMapper.setTypePrecedence(TypePrecedence.TYPE_ID);
+    // typeMapper.addTrustedPackages("*");
+    // return converter;
+
+    // }
 
     @Bean(name = "kafkaListenerContainerFactory")
-    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         log.info("setting up kafkaListenerCOntainerFactory");
+        // ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new
+        // ConcurrentKafkaListenerContainerFactory<>();
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
-        factory.setMessageConverter(messageConverter());
+        factory.setMessageConverter(new StringJsonMessageConverter());
         return factory;
     }
 }
